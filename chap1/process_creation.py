fc702903f2bb0ffa273b2a74c8f74e0dd1e9254a
fatimafarid <fatimazareen889@gmail.com>
2025-01-02 20:44:47 +0500

    Added a multiprocessing script to print the square and cube of a number using separate processes.

--- chap1/process_creation.py
+++ chap1/process_creation.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from multiprocessing import Process
 
 def print_square(num):
@@ -18,24 +18,3 @@ if __name__ == "__main__":
     p2.join()
 
     print("Processes finished executing")
-=======
-from multiprocessing import Process
-
-def print_square(num):
-    print(f"Square: {num * num}")
-
-def print_cube(num):
-    print(f"Cube: {num * num * num}")
-
-if __name__ == "__main__":
-    p1 = Process(target=print_square, args=(10,))
-    p2 = Process(target=print_cube, args=(10,))
-
-    p1.start()
-    p2.start()
-
-    p1.join()
-    p2.join()
-
-    print("Processes finished executing")
->>>>>>> 15be036 (Added chap3 to chap9 folders)
