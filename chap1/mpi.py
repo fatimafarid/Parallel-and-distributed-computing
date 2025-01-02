b10efcc779a051e8cdfad9d9e25b9601aadb5f9a
fatimafarid <fatimazareen889@gmail.com>
2025-01-02 20:40:47 +0500

    Added an MPI script to demonstrate data sending and receiving between two processes, with idle handling for others.

--- chap1/mpi.py
+++ chap1/mpi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#code7
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
@@ -17,23 +17,3 @@ else:
         print(f"Received data at process 1: {data}")
     else:
         print(f"Process {rank} is idle.")  # Print message for idle processes
-=======
-from mpi4py import MPI
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()  # Get the total number of processes
-
-if size < 2:
-    print("This program requires at least 2 processes.")
-else:
-    if rank == 0:
-        data = {'key1': 'value1', 'key2': 'value2'}
-        comm.send(data, dest=1)  # Send data to process with rank 1
-        print("Sent data from process 0")
-    elif rank == 1:
-        data = comm.recv(source=0)  # Receive data from process with rank 0
-        print(f"Received data at process 1: {data}")
-    else:
-        print(f"Process {rank} is idle.")  # Print message for idle processes
->>>>>>> 15be036 (Added chap3 to chap9 folders)
