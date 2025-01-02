6da1ba2d6b1d97b723e1c09d06140377d4f6ac70
fatimafarid <fatimazareen889@gmail.com>
2025-01-02 20:36:44 +0500

    Added a multithreading script to calculate the Fibonacci sequence and measure execution time.

--- chap1/fibonacci.py
+++ chap1/fibonacci.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#code4
 import threading
 import time
 
@@ -22,28 +22,3 @@ for t in threads:
     
 end_time = time.time()
 print(f"threads execution time:{end_time - start_time} seconds")
-=======
-import threading
-import time
-
-def calc_fibonacci(n):
-    if n == 0 or n == 1:
-        return n
-    else:
-        return calc_fibonacci(n-1) + calc_fibonacci(n-2)
-
-threads = []
-start_time = time.time()
-for i in range(9):  # 4 threads
-    t = threading.Thread(target=calc_fibonacci, args=(35,))
-    threads.append(t)
-
-    print(f"Starting thread {i + 1}")
-    t.start()
-
-for t in threads:
-    t.join()
-    
-end_time = time.time()
-print(f"threads execution time:{end_time - start_time} seconds")
->>>>>>> 15be036 (Added chap3 to chap9 folders)
