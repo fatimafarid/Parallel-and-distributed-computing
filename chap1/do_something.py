5701e8170ce7e3f396d948fe0b2edc823ee5d250
fatimafarid <fatimazareen889@gmail.com>
2025-01-02 20:34:05 +0500

    for generating random floating-point numbers and appending them to a list.

--- chap1/do_something.py
+++ chap1/do_something.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
+#code3
 import random
 def do_something(count,out_list):
     for i in range(count):
-=======
-import random
-def do_something(count,out_list):
-    for i in range(count):
->>>>>>> 15be036 (Added chap3 to chap9 folders)
+
         out_list.append(random.random())
